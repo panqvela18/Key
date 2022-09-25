5e21da83a3d2aaa56b9640e2f8fad8ab75ca8823
panqvela18 <irakli.pankvelashvili@gmail.com>
2022-09-26 03:40:24 +0400

    Key WithJS

---
+++ app.js
@@ -0,0 +1,18 @@
+const isnert = document.getElementById('insert');
+
+
+window.addEventListener('keydown',(event) => {;
+    isnert.innerHTML = `
+        <div class="key">
+            ${event.key  === ' ' ? 'Space' : event.key}
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            ${event.keyCode}
+            <small>event.keycode</small>
+        </div>
+        <div class="key">
+            ${event.code}
+            <small>event.code</small>
+        </div>`
+})
